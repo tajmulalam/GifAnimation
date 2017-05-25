cdf0630c11667a2b3bf7cf5e0cbcb90df03f5886
sauravMir <saurav_41@yahoo.com>
2017-05-25 17:31:34 +0600

    push

--- app/src/main/java/com/example/sumon/videosplash/WelcomeActivity.java
+++ app/src/main/java/com/example/sumon/videosplash/WelcomeActivity.java
@@ -4,7 +4,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Toast;
 
+import java.io.IOException;
+
+import pl.droidsonroids.gif.AnimationListener;
+import pl.droidsonroids.gif.GifDrawable;
 import pl.droidsonroids.gif.GifImageView;
 
 public class WelcomeActivity extends AppCompatActivity {
@@ -16,6 +21,27 @@ public class WelcomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_welcome);
         ivMonster = (GifImageView) findViewById(R.id.ivMonster);
         ivFood = (GifImageView) findViewById(R.id.ivFood);
+
+        try {
+            final GifDrawable gifFromResourceMons = new GifDrawable( getResources(), R.drawable.greenbar);
+            ivMonster.setImageDrawable(gifFromResourceMons);
+
+            final GifDrawable gifFromResourceFood = new GifDrawable( getResources(), R.drawable.redbar);
+            ivFood.setImageDrawable(gifFromResourceFood);
+
+            //gifFromResource.stop();
+           // gifFromResourceMons.setLoopCount(1);
+            gifFromResourceMons.addAnimationListener(new AnimationListener() {
+                @Override
+                public void onAnimationCompleted(int loopNumber) {
+                    //Toast.makeText(WelcomeActivity.this,"sada",Toast.LENGTH_LONG).show();
+                }
+            });
+          //  gifFromResourceFood.setLoopCount(1);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 
